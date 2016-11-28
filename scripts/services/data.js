ee08c874e02ec93082db75c089b6bf98b8225ae1
Sam Cutler <Sam Cutler>
2016-11-28 07:11:06 -0700

    First commit

---
+++ scripts/services/data.js
@@ -0,0 +1,20 @@
+"use strict";
+
+angular.module("todoListApp")
+
+.service("dataService", function($http) {
+
+	this.getTodos = function(callback) {
+		$http.get("mock/todos.json").then(callback)
+	}
+
+	this.deleteTodo = function(todo) {
+		console.log("The " + todo.name + " todo has been deleted!");
+		// other logic
+	};
+
+	this.saveTodo = function(todo) {
+		console.log("The " + todo.name + " todo has been saved!");
+		// other logic
+	};
+});
